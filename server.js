d7ce99211139c3c2bbbed802b9949750627aac00
TahinDani <tahin.daniel@gmail.com>
2018-07-02 14:31:13 +0200

    database config changed for heroku

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const port = process.env.PORT || 3000;
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'tahin',
-      password : 'tahin',
-      database : 'smart-brain'
+      connectionString : process.env.DATABASE_URL,
+      ssl: true,
     }
   });
 
