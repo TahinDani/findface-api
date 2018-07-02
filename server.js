c4940b42292987be645fdb83dc68207202113bf9
TahinDani <tahin.daniel@gmail.com>
2018-07-02 13:13:05 +0200

    port changed

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+const port = process.env.PORT || 3000;
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -25,16 +27,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send(database.users);
-});
-
+app.get("/", (req, res) => { res.send('HOME') });
 app.post('/signin', signin.handleSignin(db, bcrypt)); // The advanced way
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/image/predict', (req, res) => { image.predict(req, res) });
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000');
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
 });
